903abdbbac5692f21d68cdd7231599fd1b1854ef
251484 Binkowska Maja <251484@edu.p.lodz.pl>
2025-04-28 14:54:17 +0200

    Dodanie zbeszczeszczonego przez rebase zapętlenia main

--- main.py
+++ main.py
@@ -66,84 +66,92 @@ funkcje = {
 
 nieIstnieje = None
 
-# wybor funkcji przez uzytkownika
-print("Wybierz funkcje:")
-for wybor in funkcje:
-    print(f"{wybor}: {funkcje[wybor][0]}")
+petla = True
 
-# zabezpieczenie przez wprowadzaniem zlego znaku albo nieistniejacej funkcji
-wyborFunkcji = input("Podaj numer funkcji: ")
-dostepneWybory = [1, 2, 3, 4, 5]
+while True:
+    # wybor funkcji przez uzytkownika
+    print("Wybierz funkcje:")
+    for wybor in funkcje:
+        print(f"{wybor}: {funkcje[wybor][0]}")
 
-# powtarzaj prosbe o podanie funkcji z zakresu
-while (wyborFunkcji.isdigit() == False) or (int(wyborFunkcji) not in dostepneWybory):
-    print("Podaj liczbę całkowitą z przedziału 1-5!")
+    print("0. Zakończ program")
+
     wyborFunkcji = input("Podaj numer funkcji: ")
+    dostepneWybory = [0, 1, 2, 3, 4, 5]
 
-# zapisz funkcje podana przez uzytkownika
-wyborFunkcji = int(wyborFunkcji)
+    if wyborFunkcji == "0":
+        exit(0)
 
-# zabezpieczenie przed podaniem niewlasciwego przedzialu
-poczatekPrzedzialu = input("Podaj początek przedziału: ")
-while zabezpieczenie(poczatekPrzedzialu) == False:
+    # zabezpieczenie przez wprowadzaniem zlego znaku albo nieistniejacej funkcji
+    # powtarzaj prosbe o podanie funkcji z zakresu
+    while (wyborFunkcji.isdigit() == False) or (int(wyborFunkcji) not in dostepneWybory):
+        print("Podaj liczbę całkowitą z przedziału 1-5!")
+        wyborFunkcji = input("Podaj numer funkcji: ")
+
+    # zapisz funkcje podana przez uzytkownika
+    wyborFunkcji = int(wyborFunkcji)
+
+    # zabezpieczenie przed podaniem niewlasciwego przedzialu
     poczatekPrzedzialu = input("Podaj początek przedziału: ")
+    while zabezpieczenie(poczatekPrzedzialu) == False:
+        poczatekPrzedzialu = input("Podaj początek przedziału: ")
 
-koniecPrzedzialu = input("Podaj koniec przedziału: ")
-while zabezpieczenie(koniecPrzedzialu) == False:
     koniecPrzedzialu = input("Podaj koniec przedziału: ")
+    while zabezpieczenie(koniecPrzedzialu) == False:
+        koniecPrzedzialu = input("Podaj koniec przedziału: ")
 
-print("\n-------------\n"
-      "Wybrana funkcja:", funkcje[wyborFunkcji][0],
-      f"\nWybrany przedział: [{poczatekPrzedzialu}, {koniecPrzedzialu}]"
-      f"\n-------------\n")
+    print("\n-------------\n"
+          "Wybrana funkcja:", funkcje[wyborFunkcji][0],
+          f"\nWybrany przedział: [{poczatekPrzedzialu}, {koniecPrzedzialu}]"
+          f"\n-------------\n")
 
-# zapisz przedzialy jako float
-poczatekPrzedzialu = float(poczatekPrzedzialu)
-koniecPrzedzialu = float(koniecPrzedzialu)
+    # zapisz przedzialy jako float
+    poczatekPrzedzialu = float(poczatekPrzedzialu)
+    koniecPrzedzialu = float(koniecPrzedzialu)
 
-# zamien ze soba koniec i poczatek przedzialu, jezeli poczatek przedzialu jest dalej niz jego koniec
-if koniecPrzedzialu <= poczatekPrzedzialu:
-    koniecPrzedzialu, poczatekPrzedzialu = poczatekPrzedzialu, koniecPrzedzialu
+    # zamien ze soba koniec i poczatek przedzialu, jezeli poczatek przedzialu jest dalej niz jego koniec
+    if koniecPrzedzialu <= poczatekPrzedzialu:
+        koniecPrzedzialu, poczatekPrzedzialu = poczatekPrzedzialu, koniecPrzedzialu
 
-funkcjaWybrana = funkcje[wyborFunkcji][1]
+    funkcjaWybrana = funkcje[wyborFunkcji][1]
 
-print("Podaj w jakiej formie chcesz podać wartości x węzłów")
+    print("Podaj w jakiej formie chcesz podać wartości x węzłów")
 
-print("\n-------------\n"
-      "Podaj w jakiej formie chcesz podać wartości x węzłów\n"
-      "1. Przez plik\n"
-      "2. Przez teminal"
-      f"\n-------------\n")
-wyborWezlow = int(input("Wybór(1/2): "))
+    print("\n-------------\n"
+          "Podaj w jakiej formie chcesz podać wartości x węzłów\n"
+          "1. Przez plik\n"
+          "2. Przez teminal"
+          f"\n-------------\n")
+    wyborWezlow = int(input("Wybór(1/2): "))
 
-if wyborWezlow == 1:
-    nazwaPliku = input("Podaj nazwe pliku: ")
-    # nazwaPliku += ".txt"
-    if not os.path.exists(nazwaPliku):
-        print("Podany plik nie istnieje!")
-        exit(1)
-    wezlyX = wczytanie_wartosci(nazwaPliku)
-elif wyborWezlow == 2:
-    # zapytanie do uzytkownika o podanie wezlow po przecinku
-    wezlyX = input("Podaj po przecinku wartosci x węzłów (np. 1,2,3): ").split(",")
-else:
-    print("Podano złą wartość!")
-    exit(1)
-
-# utworz slownik na wezly
-wezly = {}
-
-# zapisz wartosci y odpowiadajace argumentom x
-for wezel in wezlyX:
-    if float(wezel) <  poczatekPrzedzialu or float(wezel) > koniecPrzedzialu:
-        print(f"Podany węzeł {wezel} wychodzi poza zadany przedział, wartość zostaje pominięta!")
+    if wyborWezlow == 1:
+        nazwaPliku = input("Podaj nazwe pliku: ")
+        # nazwaPliku += ".txt"
+        if not os.path.exists(nazwaPliku):
+            print("Podany plik nie istnieje!")
+            exit(1)
+        wezlyX = wczytanie_wartosci(nazwaPliku)
+    elif wyborWezlow == 2:
+        # zapytanie do uzytkownika o podanie wezlow po przecinku
+        wezlyX = input("Podaj po przecinku wartosci x węzłów (np. 1,2,3): ").split(",")
     else:
-        wezly[float(wezel)] = float(funkcjaWybrana(float(wezel)))
+        print("Podano złą wartość!")
+        exit(1)
 
-if not wezly:
-    print("Podano brak węzłów!")
-    exit(1)
+    # utworz slownik na wezly
+    wezly = {}
 
-print("[Wyniki]")
+    # zapisz wartosci y odpowiadajace argumentom x
+    for wezel in wezlyX:
+        if float(wezel) <  poczatekPrzedzialu or float(wezel) > koniecPrzedzialu:
+            print(f"Podany węzeł {wezel} wychodzi poza zadany przedział, wartość zostaje pominięta!")
+        else:
+            wezly[float(wezel)] = float(funkcjaWybrana(float(wezel)))
 
-rysowanieFunkcji(funkcjaWybrana, poczatekPrzedzialu, koniecPrzedzialu)
+    if not wezly:
+        print("Podano brak węzłów!")
+        exit(1)
+
+    print("[Wyniki]")
+
+    rysowanieFunkcji(funkcjaWybrana, poczatekPrzedzialu, koniecPrzedzialu)
